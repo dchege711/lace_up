9567fc747537cba6e2d780656a5b84af6abb20d8
dchege711 <d.chege711@gmail.com>
2018-04-23 17:35:55 -0400

    Adapt the MongoDB clint for sport_together

--- back_end_scripts/mongo_db_client.py
+++ back_end_scripts/mongo_db_client.py
@@ -1,4 +1,8 @@
 """
+mongo_db_client.py
+
+A wrapper around the MongoDB database hosting game and account
+information.
 
 """
 
@@ -7,21 +11,30 @@
 import os
 from pymongo import MongoClient, IndexModel, TEXT, DESCENDING, HASHED
 from pprint import pprint
+import secrets
+import string
 from bson.son import SON
 
 #_______________________________________________________________________________
 
 indexes = {
-	"attendee_details": [
-		("Email", HASHED), 
+	"sport_together_user_account_info": [
+		("user_id", HASHED), ("location", TEXT)
 	],
-	"trip_details": [
-		("origin", TEXT), ("departure_date", DESCENDING),
-		("departure_time", DESCENDING), ("seats_available", DESCENDING)
+	"sport_together_user_logins": [
+		("email", HASHED), ("salt", TEXT)
+	],
+	"sport_together_game_details": [
+		("location", TEXT), ("time", DESCENDING)
 	]
 }
 
-class tiger_rides_db():
+class sport_together_db():
+	"""
+	A wrapper around the MongoDB database used for the Sport 
+	Together application.
+	
+	"""
 	
 	def __init__(self, collection_name):
 		self.client = MongoClient(os.environ["TIGER_RIDES_MONGO_URI"])
@@ -41,16 +54,6 @@ class tiger_rides_db():
 		return self.collection.update_one(
 			filter_key, {"$set": key_value_pairs}
 		)
-			
-		
-	def get_next_id(self):
-		metadata = self.collection.find_one({"document_type": "metadata"})
-		next_id = metadata["next_id"]
-		self.update(
-			{"_id": metadata["_id"]},
-			{"next_id": next_id + 1}
-		)
-		return next_id
 		
 	def read(self, query):
 		return self.collection.find_one(filter=query)
@@ -59,7 +62,7 @@ class tiger_rides_db():
 		return self.collection.delete_one(query_filter)
 		
 def main():
-	tiger_rides = tiger_rides_db("attendee_details")
+	sport_together = sport_together_db("user_account_info")
 	# tiger_rides._initialize()
 	# tiger_rides.create({
 	# 	"email": "nothing@gmail.com",
@@ -71,10 +74,6 @@ def main():
 	# 	"has_car": "no",
 	# 	"password": "lets_save_passwords_in_plain_text"
 	# })
-	print(tiger_rides.get_next_id())
-	
-	
-	
 
 if __name__ == "__main__":
 	main()
